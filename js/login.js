f102fcb83e76324d33b01affe2eb3f25ade3c30f
Cfauto <cfautoeclasse5a@gmail.com>
2026-04-30 19:06:34 +0200

    Reworded the login prompt again

--- js/login.js
+++ js/login.js
@@ -13,6 +13,7 @@ jslogin.appendChild(jslogin2)
 
 function loadlogin() {
     loadedlogin = true;
+    jslogin.innerHTML = "Login succeful. Download links enabled. Go to the downloads page the download the and play."
     fancymenu.placeholders.get('playername')
     .then(name => {
         jslogin2.innerHTML = "Connected to main servers.<br>Logged in as " + name + "<br>.Download the launcher to start plaing."

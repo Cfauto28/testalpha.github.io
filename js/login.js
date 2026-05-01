777c61a72d55c5f7d98a79b9ef5d9db3097b7868
Cfauto <cfautoeclasse5a@gmail.com>
2026-05-01 21:10:50 +0200

    Removed fancymenu support

--- js/login.js
+++ js/login.js
@@ -16,21 +16,6 @@ if (navigator.userAgent.includes("Trident")){
 }
 jslogin.appendChild(jslogin2)
 
-function loadlogin() {
-    loadedlogin = true;
-    jslogin.innerHTML = "Login succeful. Download links enabled. Go to the downloads page the download the and play."
-    fancymenu.placeholders.get('playername')
-    .then(name => {
-        jslogin2.innerHTML = "Connected to main servers.<br>Logged in as " + name + "<br>.Download the launcher to start plaing."
-    }).catch(error => console.error(error));;
-};
-
-window.addEventListener('fancymenu-ready', () => {
-        console.log('FancyMenu API is ready');
-        if (loadedlogin !== true){
-            loadlogin();
-        };
-});
 
 
 /*	<div class="login">

855b5ae928c4a193ff9d60397ff23db5a3cf595c
Cfauto <cfautoeclasse5a@gmail.com>
2026-05-03 19:17:13 +0200

    IE compatability test 3

--- js/login.js
+++ js/login.js
@@ -11,10 +11,6 @@ document.getElementById("loginspace").appendChild(jslogin);
 
 var jslogin2 = document.createElement('div');
 jslogin2.className = "login2";
-if (navigator.userAgent.endsWith("Trident/7.0; rv:11.0) like Gecko")){
-    jslogin2.innerHTML = "Unable to initialize the ActiveX control.<br>Check if the main servers are online<br>Also try refreshing the page.";
-} else {
-  jslogin2.innerHTML = "Unable to initialize login prompt.<br>Check if your browser supports NPAPI <br>Also try refreshing the page.";
-}
+jslogin2.innerHTML = "Unable to initialize login prompt.<br>Check if your browser supports NPAPI or ActiveX <br>Also try refreshing the page.";
 jslogin.appendChild(jslogin2)
 

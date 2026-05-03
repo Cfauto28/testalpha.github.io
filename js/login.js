a2411259a4e57f79db6fcd9d9e434631d16c0f97
Cfauto <cfautoeclasse5a@gmail.com>
2026-05-03 18:47:53 +0200

    IE compatability test 1

--- js/login.js
+++ js/login.js
@@ -2,17 +2,17 @@
 
 // Top Part
 
-let jslogin = document.createElement('div');
+var jslogin = document.createElement('div');
 jslogin.className = "login";
 jslogin.innerHTML = "Please log in here with your Minecraft account to enable download links.<br><br>";
 document.querySelector(".mwidthsection").appendChild(jslogin);
 
 //Bottom Part
 
-let jslogin2 = document.createElement('div');
+var jslogin2 = document.createElement('div');
 jslogin2.className = "login2";
 if (navigator.userAgent.includes("Trident")){
-    jslogin2.innerHTML = "Unable to initialize the ActiveX control.<br>Check if the main servers are online<br>Also try refreshing the page."
+    jslogin2.innerHTML = "Unable to initialize the ActiveX control.<br>Check if the main servers are online<br>Also try refreshing the page.";
 } else {
   jslogin2.innerHTML = "Unable to initialize login prompt.<br>Check if your browser supports NPAPI <br>Also try refreshing the page.";
 }

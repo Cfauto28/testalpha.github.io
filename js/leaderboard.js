44cd7f3da0d62294ee7830cc730c10598b842b06
Cfauto <cfautoeclasse5a@gmail.com>
2026-05-10 20:12:15 +0200

    Added the leaderboard shown in Clock Reset

---
+++ js/leaderboard.js
@@ -0,0 +1,190 @@
+'use strict';
+
+if ("content" in document.createElement("template")) {
+    const tbody = document.getElementById("speedrunrowdom");
+    const template = document.getElementById("speedrunrow");
+
+    const clone = document.importNode(template.content, true);
+    var td = clone.querySelectorAll("td");
+    td[0].innerHTML = "FordwarddSlash/<span class=uid>4b3b-9d32</span>"
+    td[1].textContent = "00.08.100"
+    td[2].innerHTML = "<a href=g_404.html>Demo (23.8KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone);
+
+    const clone2 = document.importNode(template.content, true);
+    td = clone2.querySelectorAll("td");
+    td[0].innerHTML = "---/<span class=uid>1650-fd65</span>"
+    td[1].textContent = "00.08.850"
+    td[2].innerHTML = "<a href=g_404.html>Demo (24.1KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone2);
+
+    const clone3 = document.importNode(template.content, true);
+    td = clone3.querySelectorAll("td");
+    td[0].innerHTML = "---/<span class=uid>456b-923a</span>"
+    td[1].textContent = "00.09.750"
+    td[2].innerHTML = "<a href=g_404.html>Demo (26.21KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone3);
+
+    const clone4 = document.importNode(template.content, true);
+    td = clone4.querySelectorAll("td");
+    td[0].innerHTML = "---/<span class=uid>c7ad-0566</span>"
+    td[1].textContent = "00.16.050"
+    td[2].innerHTML = "<a href=g_404.html>Demo (43.08KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone4);
+
+
+    const clone5 = document.importNode(template.content, true);
+    td = clone5.querySelectorAll("td");
+    td[0].innerHTML = "hirakao00/<span class=uid>20df-0b5a</span>"
+    td[1].textContent = "00.17.100"
+    td[2].innerHTML = "<a href=g_404.html>Demo (44.82KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone5);
+
+    const clone6 = document.importNode(template.content, true);
+    td = clone6.querySelectorAll("td");
+    td[0].innerHTML = "Astralily/<span class=uid>e4d1-de5a</span>"
+    td[1].textContent = "00.17.350"
+    td[2].innerHTML = "<a href=g_404.html>Demo (45.1KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone6);
+
+    const clone7 = document.importNode(template.content, true);
+    td = clone7.querySelectorAll("td");
+    td[0].innerHTML = "HooHooYee9/<span class=uid>5858-733e</span>"
+    td[1].textContent = "00.17.600"
+    td[2].innerHTML = "<a href=g_404.html>Demo (46.1KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone7);
+
+    const clone8 = document.importNode(template.content, true);
+    td = clone8.querySelectorAll("td");
+    td[0].innerHTML = "TheDev27/<span class=uid>cfec-b027</span>"
+    td[1].textContent = "00.18.000"
+    td[2].innerHTML = "<a href=g_404.html>Demo (46.9KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone8);
+
+    const clone9 = document.importNode(template.content, true);
+    td = clone9.querySelectorAll("td");
+    td[0].innerHTML = "carlotgaming/<span class=uid>0435-bd9a</span>"
+    td[1].textContent = "00.18.210"
+    td[2].innerHTML = "<a href=g_404.html>Demo (47.2KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone9);
+
+    const clone10 = document.importNode(template.content, true);
+    td = clone10.querySelectorAll("td");
+    td[0].innerHTML = "r3plan77/<span class=uid>b1b0-b509</span>"
+    td[1].textContent = "00.18.900"
+    td[2].innerHTML = "<a href=g_404.html>Demo (48.3KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone10);
+
+    const clone11 = document.importNode(template.content, true);
+    td = clone11.querySelectorAll("td");
+    td[0].innerHTML = "Dreivier/<span class=uid>37ca-76a0</span>"
+    td[1].textContent = "00.19.200"
+    td[2].innerHTML = "<a href=g_404.html>Demo (48.72KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone11);
+
+    const clone12 = document.importNode(template.content, true);
+    td = clone12.querySelectorAll("td");
+    td[0].innerHTML = "unfortun8/<span class=uid>8db7-23aa</span>"
+    td[1].textContent = "00.19.450"
+    td[2].innerHTML = "<a href=g_404.html>Demo (49.4KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone12);
+
+    const clone13 = document.importNode(template.content, true);
+    td = clone13.querySelectorAll("td");
+    td[0].innerHTML = "7isage/<span class=uid>8160-7dfa</span>"
+    td[1].textContent = "00.19.500"
+    td[2].innerHTML = "<a href=g_404.html>Demo (51.34KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone13);
+
+    const clone14 = document.importNode(template.content, true);
+    td = clone14.querySelectorAll("td");
+    td[0].innerHTML = "IceWolf/<span class=uid>ad8b-7d86</span>"
+    td[1].textContent = "00.19.630"
+    td[2].innerHTML = "<a href=g_404.html>Demo (52.48KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone14);
+
+    const clone15 = document.importNode(template.content, true);
+    td = clone15.querySelectorAll("td");
+    td[0].innerHTML = "racinggaming6/<span class=uid>68d9-00aa</span>"
+    td[1].textContent = "00.19.900"
+    td[2].innerHTML = "<a href=g_404.html>Demo (53.0KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone15);
+
+    const clone16 = document.importNode(template.content, true);
+    td = clone16.querySelectorAll("td");
+    td[0].innerHTML = "S8alite/<span class=uid>5841-0645</span>"
+    td[1].textContent = "00.20.560"
+    td[2].innerHTML = "<a href=g_404.html>Demo (56.0KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone16);
+
+    const clone17 = document.importNode(template.content, true);
+    td = clone17.querySelectorAll("td");
+    td[0].innerHTML = "Vandsow/<span class=uid>1f8e-d039</span>"
+    td[1].textContent = "00.20.800"
+    td[2].innerHTML = "<a href=g_404.html>Demo (56.2KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone17);
+
+    const clone18 = document.importNode(template.content, true);
+    td = clone18.querySelectorAll("td");
+    td[0].innerHTML = "MrCreeperZombie/<span class=uid>7884-36d4</span>"
+    td[1].textContent = "00.20.840"
+    td[2].innerHTML = "<a href=g_404.html>Demo (56.24KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone18);
+
+    const clone19 = document.importNode(template.content, true);
+    td = clone19.querySelectorAll("td");
+    td[0].innerHTML = "_slabsalesman_/<span class=uid>c40e-3ed0</span>"
+    td[1].textContent = "00.21.400"
+    td[2].innerHTML = "<a href=g_404.html>Demo (63.76KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone19);
+
+    const clone20 = document.importNode(template.content, true);
+    td = clone20.querySelectorAll("td");
+    td[0].innerHTML = "skymuvth/<span class=uid>cad9-a3db</span>"
+    td[1].textContent = "00.21.960"
+    td[2].innerHTML = "<a href=g_404.html>Demo (64.1KB)</a>"
+    td[3].innerHTML = "<a href=g_404.html>Report</a"
+
+    tbody.appendChild(clone20);
+} else {
+    console.error("Your browser is shit")
+}
+
